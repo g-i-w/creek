ce950c96016989decdd3e03e4474e29ad0e607c3
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2024-01-09 14:40:01 -0600

    undoing the info methods added to Table.java

--- Table.java
+++ Table.java
@@ -3,28 +3,23 @@ package creek;
 import java.util.*;
 
 public interface Table {
-	// Dangerous methods
+	// Not restricted and not thread-safe
 	public Table data ( List<List<String>> data );
 	public List<List<String>> data ();
 	
-	public Table info ( List<List<Map<String,String>>> info );
-	public List<List<Map<String,String>>> info ();
-	
-	// Thread-safe methods
 	public List<List<String>> last ( int lastRows );
 	public List<List<String>> slice ( int startRowInclusive, int endRowExclusive );
 	public List<List<String>> slice ( int startRowInclusive, int endRowExclusive, int startColInclusive, int endColExclusive );
 		
+	// Simple, restricted, and mostly thread-safe
 	public String item ( int row, int col );
 	public String[] row ( int row );
 	public String[] col ( int col );
 	
-	public Map<String,String> itemInfo ( int row, int col, String key );
-	public Table itemInfo ( int row, int col, String key, String val );
-	
 	public int rowCount ();
 	public int colCount ( int row );
 
+	// Write-locked and thread-safe
 	public Table alias ( Table table );
 	public Table copy ( Table table );
 	public Table append ( Table table );
